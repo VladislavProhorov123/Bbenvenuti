af878de1db74104368d2d13e1022dd6b52653c8b
Vladislav <vladislavprokhorov1005@gmail.com>
2026-01-11 16:35:48 +0200

    Features Swiper cards

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 export default function ProductCard({product}) {
   return (
-    <Link to={`/products/${product.id}`}>
-    <div className="border p-4 rounded-lg hover:shadow-lg transition">
+    <Link to={`/product/${product.id}`}>
+    <div className="border p-4 rounded-lg hover:shadow-lg transition w-[300px] h-[340px]">
         <img src={product.image} className="h-60 w-full object-cover" />
         <h3 className="mt-2 font-semibold">{product.title}</h3>
         <p>${product.price}</p>
--- src/sections/Featured.jsx
+++ src/sections/Featured.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import "swiper/css/navigation";
 
 import { products } from "../data/products";
 import ProductCard from "../components/ProductCard";
 import SectionTitle from "../components/SectionTitle";
 import { useNavigate } from "react-router-dom";
+import { Autoplay, Navigation } from "swiper/modules";
 
 export default function FeaturedProducts() {
   const navigate = useNavigate();
 
   return (
-    <section className="py-24 bg-[var(--bg-secondary)]">
+    <section className="py-24 bg-[var(--bg-secondary)] relative">
       <div className="container">
         <SectionTitle
           title="Featured Fragrances"
@@ -19,8 +21,14 @@ export default function FeaturedProducts() {
         />
 
         <Swiper
-          spaceBetween={24}
-          slidesPerView={4}
+          className="w-[1280px]"
+          modules={[Navigation, Autoplay]}
+          navigation={{
+            prevEl: ".custom-prev",
+            nextEl: ".custom-next",
+          }}
+          autoplay={{ delay: 5000, disableOnInteraction: false }}
+          slidesPerView={3}
           breakpoints={{
             320: { slidesPerView: 1.2 },
             768: { slidesPerView: 2.2 },
@@ -34,10 +42,18 @@ export default function FeaturedProducts() {
           ))}
         </Swiper>
 
+        <button className="custom-prev absolute left-[50px] top-1/2 -translate-y-1/2 z-20 w-12 h-12 rounded-full bg-[var(--brand-primary)] text-white flex items-center justify-center shadow-lg hover:bg-[var(--brand-accent)] transition-transform duration-300 hover:scale-110 cursor-pointer">
+            ‹
+          </button>
+          <button className="custom-next absolute right-[70px] top-1/2 -translate-y-1/2 z-20 w-12 h-12 rounded-full bg-[var(--brand-primary)] text-white flex items-center justify-center shadow-lg hover:bg-[var(--brand-accent)] transition-transform duration-300 hover:scale-110">
+            ›
+          </button>
+
+
         <div className="flex justify-center mt-14">
           <button
             onClick={() => navigate("/products")}
-            className="px-10 py-3 rounded-full bg-[var(--brand-primary)] hover:bg-[var(--brand-accent)] text-white font-semibold transition"
+            className="px-10 py-3 rounded-full bg-[var(--brand-primary)] hover:bg-[var(--brand-accent)] text-white font-semibold transition cursor-pointer"
           >
             Shop Now
           </button>
